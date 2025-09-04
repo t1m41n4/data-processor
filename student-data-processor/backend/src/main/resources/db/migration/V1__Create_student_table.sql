3a08d1de358b4cdcaf3181e72c0f498c1bedfc1e
Tim <timmaina64@gmail.com>
2025-09-04 15:29:53 +0300

    Delete student-data-processor/backend/src/main/resources/db/migration/V1__Create_student_table.sql

--- student-data-processor/backend/src/main/resources/db/migration/V1__Create_student_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE student (
-    student_id SERIAL PRIMARY KEY,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    dob DATE NOT NULL,
-    class VARCHAR(20) NOT NULL,
-    score DECIMAL(5, 2) NOT NULL
-);
